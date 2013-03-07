f17f39a9e1832acf0dab4f3ae9d8926a0349ecb0
Ali Servet Donmez <asd@pittle.org>
2013-03-07 14:41:58 +0000

    Changed AssemblyInfo

--- ReusableApplications/Properties/AssemblyInfo.cs
+++ ReusableApplications/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ReusableApplications")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DRY Applications")]
+[assembly: AssemblyDescription("Don't Repeat Yourself")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Apex-net S.r.l.")]
 [assembly: AssemblyProduct("ReusableApplications")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright ©  2012, 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
